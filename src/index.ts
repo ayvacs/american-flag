bd09f547820509095633d5e215f8625e59fc4a06
ayvacs <ayvacs@proton.me>
2024-07-17 01:45:29 -0400

    feat: some fixes, star generation

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,9 @@ const validCanvasDimensionsFt = [
     [1.32,  2.50]
 ];
 
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 function generateRandomRed(): string {
     const r = 255                           .toString(16).padStart(2, "0");
     const g = Math.floor(Math.random() * 96).toString(16).padStart(2, "0");
@@ -29,9 +32,17 @@ function generateRandomRed(): string {
     return `#${r}${g}${b}`;
 };
 
+function generateRandomBlue(): string {
+    const r = Math.floor(Math.random() * 96).toString(16).padStart(2, "0");
+    const g = Math.floor(Math.random() * 96).toString(16).padStart(2, "0");
+    const b = 255                           .toString(16).padStart(2, "0");
+
+    return `#${r}${g}${b}`;
+};
 
 
-async function createFlag() {
+
+async function createFlag(outputNum: number) {
     // determine random flag dimensions
     const [widthFt, lengthFt] = validCanvasDimensionsFt[Math.floor(Math.random() * validCanvasDimensionsFt.length)];
 
@@ -44,19 +55,19 @@ async function createFlag() {
     const canvas = Canvas.createCanvas(length, width);
     const flag = canvas.getContext("2d");
 
-    // fill the canvas with white
-    flag.fillStyle = "white";
-    flag.fillRect(0, 0, canvas.width, canvas.height);
-
 
     // functions
     function drawStripes(count: number) {
-        flag.fillStyle = generateRandomRed();
+        flag.fillStyle = Math.random() < 0.5 ? "white" : generateRandomRed(); // randomly choose to start at either red or white
         let yPos = 0; // first stripe starts at y=0
 
         for (let i = 0; i < count; i++) {
             // determine the height of the stripe (do I really need to comment this?)
-            const stripeHeight = (Math.random() * (canvas.height - yPos)) / 3;
+            let stripeHeight;
+            if (i + 1 == count)
+                stripeHeight = canvas.height - yPos
+            else
+                stripeHeight = (Math.random() * (canvas.height - yPos)) / 3;
 
             // fill the stripe
             flag.fillRect(0, yPos, canvas.width, stripeHeight);
@@ -72,26 +83,63 @@ async function createFlag() {
         };
     };
 
+    function drawStarField(): [number, number, number, number] {
+        const padTop    = getRandomInt(10, canvas.height / 4);
+        const padRight  = getRandomInt(10, canvas.width  / 4);
+        const padBottom = getRandomInt(10, canvas.height / 4);
+        const padLeft   = getRandomInt(10, canvas.width  / 4);
+
+        const width = canvas.width - padLeft - padRight;
+        const height = canvas.height - padTop - padBottom;
+
+        flag.fillStyle = generateRandomBlue();
+        flag.fillRect(padLeft, padTop, width, height);
+        
+        return [padLeft, padTop, width, height];
+    };
+
+    async function drawStars(numStars: number, startX: number, startY: number, boundaryWidth: number, boundaryHeight: number) {
+        // load the star image
+        const starImg = await Canvas.loadImage(path.join(dirname, "../res/star.png"));
+
+        for (let i = 0; i < numStars; i++) {
+
+            const starPosX = getRandomInt(startX, startX + boundaryWidth  - (canvas.width  / 4));
+            const starPosY = getRandomInt(startY, startY + boundaryHeight - (canvas.height / 4));
+
+            const starWidth  = getRandomInt(50, boundaryWidth  - starPosX);
+            const starHeight = getRandomInt(50, boundaryHeight - starPosY);
+            
+            flag.drawImage(starImg, starPosX, starPosY, starWidth, starHeight);
+
+        };
+    };
+
+
+
     
     // The flag of the United States shall be thirteen horizontal stripes, alternate
-    // red and white; and the union of the flag shall be forty-eight stars, white in
-    // a blue field. (4 U.S.C. § 1 (2024))
+    // red and white; ... (4 U.S.C. § 1 (2024))
     drawStripes(13);
 
+    // ... and the union of the flag shall be forty-eight stars, white in a blue
+    // field. (4 U.S.C. § 1 (2024)); On the admission of a new State into the
+    // Union one star shall be added to the union of the flag ... (4 U.S.C. § 1
+    // (2024))
+    const [sfPadLeft, sfPadTop, sfWidth, sfHeight] = drawStarField();
+    await drawStars(50, sfPadLeft, sfPadTop, sfWidth, sfHeight);
 
 
 
 
     // export canvas as image
-    const pngData = await canvas.encode("png");
-    await fs.promises.writeFile(
-        path.join(
-            path.dirname(url.fileURLToPath(import.meta.url)),
-            "output.png"
-        ),
-        pngData
-    );
+    const b = canvas.toBuffer('image/png');
+    await fs.writeFileSync(path.join(dirname, `output${outputNum}.png`), b);
 };
 
 
-await createFlag();
+(async function(){
+    for (let i = 0; i < 10; i++) {
+        await createFlag(i);
+    }
+})();
